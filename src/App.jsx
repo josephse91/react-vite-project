b0f4a12a74fda2af3ff378c4e9dfc675cb98212c
victor <victorrcrispinjr@gmail.com>
2022-11-11 21:46:06 -0500

    Fix for review changes

--- src/App.jsx
+++ src/App.jsx
@@ -21,11 +21,11 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to testtt HMR
+          Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn mori
+        Click on the Vite and React logos to learn more
       </p>
 
     </div>
